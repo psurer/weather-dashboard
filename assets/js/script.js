2b3a689f1f794524530046a57d787e2296a1a2ea
psurer <surereker@icloud.com>
2021-04-17 22:52:18 -0500

    updated show date in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,110 @@
+/*
+//function to get current date
+$(document).ready(function(){
+    $("#hello2").text(moment().format("L"));
+});
+
+//function to convert temperature to Fahrenheit
+function temperatureConverter(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputFahrenheit").innerHTML=((valNum-273.15)*1.8)+32;
+    
+  }
+
+//function to get current weather
+document.getElementById("cityForm").addEventListener("submit",function(event) {
+    event.preventDefault();
+    var cityName=document.getElementById("cityNameInput").value;
+    
+    if(!cityName) {
+        alert("Please enter the name of a city.");
+    } else {
+        document.getElementById("cityName").innerText=cityName;
+
+        var requestUrl="http://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=befebd5c21afa73e074ce366b9c1c094";
+        var fiveDayRequestUrl="http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=5&appid=befebd5c21afa73e074ce366b9c1c094";
+        
+
+        fetch(requestUrl).then(function(response) {
+            if(!response.ok) {
+                alert("No information found for "+cityName);
+            } 
+            return response.json();
+          
+        }).then(function(data) {
+            console.log(data);
+
+            document.getElementById("currentTemperature").innerText=temperatureConverter(data.main.temp);
+            document.getElementById("currentHumidity").innerText=data.main.humidity;
+            document.getElementById("currentWind").innerText=data.wind.speed;
+           // document.getElementById("currentIndex").innerText=data.main.uvi;
+
+        })
+        
+       //function to get five day forecast     
+        fetch(fiveDayRequestUrl).then(function(response) {
+            return response.json();
+            }).then(function(data) {
+            console.log(data);
+   
+        var latitude=data[0].lat;
+        var longitude=data[0].lon;
+
+        var forecastRequestUrl="https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid=befebd5c21afa73e074ce366b9c1c094";
+  
+        fetch(forecastRequestUrl).then(function(response) {
+            return response.json();
+            }).then(function(data) {
+    
+            console.log(data.daily);
+
+            var forecast = ["0","1","2","3","4"];
+            console.log(forecast.length);
+
+            for(var i=0; i<data.daily.length; i++) {
+
+                console.log("Temp:"+data.daily[i]);
+
+            document.getElementById("forecastWeather1").innerText=data.daily.temp;
+            document.getElementById("forecastWeather2").innerText=data.daily.temp;
+            document.getElementById("forecastWeather3").innerText=data.daily.temp;
+            document.getElementById("forecastWeather4").innerText=data.daily.temp;
+            document.getElementById("forecastWeather5").innerText=data.daily.temp;
+            
+          
+                
+            }
+
+    })
+})
+    }
+});
+
+
+//getting dates for five day forecast
+$(document).ready(function(){
+    $("#hello3").text(moment().add( 1, "days").calender());
+});
+
+$(document).ready(function(){
+    $("#hello4").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+    $("#hello5").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+    $("#hello6").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+    $("#hello7").text(moment().format("L"));
+});
+
+
+
+*/
 /* 
     Use the current weather API (https://openweathermap.org/current)
      to display the current weather and other info of the city entered
@@ -6,6 +113,12 @@
 // look up how to add different things about the city and so on 
 // how to use latitude and longitude
 // FOR HOME WORK: current weather and forecast
+
+//function to get current date
+$(document).ready(function(){
+  $("#hello2").text(moment().format("L"));
+});
+
 
 var API_KEY="5f548afedc6a68bc83c6221ef67beab8"; 
 var city="cityNameInput"
@@ -33,98 +146,66 @@ document.getElementById("cityForm").addEventListener("submit",function(event) {
             
         }).then(function(data) {
             console.log(data);
-
+            //converts kelvin to celsius with formula of -273
             document.getElementById("currentTemperature").innerText= Math.round(data.main.temp-273.15);
             document.getElementById("currentHumidity").innerText=data.main.humidity;
+            document.getElementById("currentWindSpeed").innerText=data.wind.speed;
+            document.getElementById("uvIndex").innerText=data.main.uvi;
         })
     }
-
 })
 
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={5f548afedc6a68bc83c6221ef67beab8}
-//https://openweathermap.org/forecast5#parameter
-/*
-function getWeatherForecast(cityName) {               
-    $get("https://api.openweathermap.org/data/2.5/forecast")+cityName&API_KEY
-    //.openweathermap.org/data/2.5/weather?q=cityName&appid:{API_KEY="5f548afedc6a68bc83c6221ef67beab8"}
-var getForcast
-{
-    "coord"; {
-      "lon"; -0.13,
-      "lat"; 51.51
-    }
+fetch(fiveDayRequestUrl).then(function(response) {
+  return response.json();
+  }).then(function(data) {
+  console.log(data);
 
-    "weather"; [
-      {
-        "id": 300,
-        "main": "Drizzle",
-        "description": "light intensity drizzle",
-        "icon": "09d"
-      }
-    ],
-    "base"; "stations",
-    "main"; {
-      "temp"; 280.32,
-      "pressure"; 1012,
-      "humidity"; 81,
-      "temp_min"; 279.15,
-      "temp_max"; 281.15
-    }
-    "visibility"; 10000,
-    "wind"; {
-      "speed"; 4.1,
-      "deg"; 80
-    }
-    "clouds"; {
-      "all"; 90
-    }
-    "dt"; 1485789600,
-    "sys"; {
-      "type"; 1,
-      "id"; 5091,
-      "message"; 0.0103,
-      "country"; "GB",
-      "sunrise"; 1485762037,
-      "sunset"; 1485794875
-    }
-    "id"; 2643743,
-    "name"; "London",
-    "cod"; 200
+var latitude=data[0].lat;
+var longitude=data[0].lon;
+
+var forecastRequestUrl="https://api.openweathermap.org/data/2.5/onecall?lat="+latitude+"&lon="+longitude+"&appid=befebd5c21afa73e074ce366b9c1c094";
+
+fetch(forecastRequestUrl).then(function(response) {
+  return response.json();
+  }).then(function(data) {
+
+  console.log(data.daily);
+
+  var forecast = ["0","1","2","3","4"];
+  console.log(forecast.length);
+
+  for(var i=0; i<data.daily.length; i++) {
+
+      console.log("Temp:"+data.daily[i]);
+
+  document.getElementById("forecastWeather1").innerText=data.daily.temp;
+  document.getElementById("forecastWeather2").innerText=data.daily.temp;
+  document.getElementById("forecastWeather3").innerText=data.daily.temp;
+  document.getElementById("forecastWeather4").innerText=data.daily.temp;
+  document.getElementById("forecastWeather5").innerText=data.daily.temp;
   }
-                          
-}
-    */                  
+})
+})
 
-/* 
-
-var key = "YOUR KEY";
-var city = "YOUR CITY"; // My test case was "London"
-var url = "https://api.openweathermap.org/data/2.5/forecast";
-
-$.ajax({
-  url: url, //API Call
-  dataType: "json",
-  type: "GET",
-  data: {
-    q: city,
-    appid: key,
-    units: "metric",
-    cnt: "10"
-  },
-  success: function(data) {
-    console.log('Received data:', data) // For testing
-    var wf = "";
-    wf += "<h2>" + data.city.name + "</h2>"; // City (displays once)
-    $.each(data.list, function(index, val) {
-      wf += "<p>" // Opening paragraph tag
-      wf += "<b>Day " + index + "</b>: " // Day
-      wf += val.main.temp + "&degC" // Temperature
-      wf += "<span> | " + val.weather[0].description + "</span>"; // Description
-      wf += "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" // Icon
-      wf += "</p>" // Closing paragraph tag
-    });
-    $("#showWeatherForcast").html(wf);
-  }
+//getting dates for five day forecast
+$(document).ready(function(){
+$("#hello3").text(moment().add( 1, "days").calender());
 });
 
-*/
+$(document).ready(function(){
+$("#hello4").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+$("#hello5").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+$("#hello6").text(moment().format("L"));
+});
+
+$(document).ready(function(){
+$("#hello7").text(moment().format("L"));
+});
+
+
